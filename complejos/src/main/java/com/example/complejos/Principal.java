b461f468a1ebcf6778ec2eb6d8cf391369b91cf9
[emilioemoran] <[emilioemoran@gmail.com]>
2020-05-08 11:53:33 -0300

    Initial commit

---
+++ complejos/src/main/java/com/example/complejos/Principal.java
@@ -0,0 +1,11 @@
+package com.example.complejos;
+
+class Principal {
+    public static void main(String[] main) {
+        ComplejoAmpliado z, w;
+        z = new ComplejoAmpliado(-1.5, 3.0);
+        w = new ComplejoAmpliado(-1.2, -3.0);
+        z.suma(w);
+        System.out.println("Complejo: " + z.toString());
+    }
+}
